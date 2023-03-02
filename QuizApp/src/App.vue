e93500021c94f835174b1f34c7e2d99dbe9310c3
Quynh <ngophuongquynh25@gmail.com>
2023-03-02 11:43:04 +0100

    Separating Code Into Components

--- QuizApp/src/App.vue
+++ QuizApp/src/App.vue
@@ -1,23 +1,30 @@
 <script setup>
-import q from "./data/quizes.json"
-import {ref, watch} from "vue"
-const quizes = ref(q)
-const search =ref("")
-watch(search, () =>{
-  console.log("hello from watch")
-  quizes.value= q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
-})
+import q from "./data/quizes.json";
+import { ref, watch } from "vue";
+// Listening to Changes With Watch
+import Card from "./components/Card.vue";
+
+const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  console.log("hello from watch");
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+
 </script>
 
 <template>
   <div class="container">
     <header>
-      
       <h1>Quizes</h1>
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img
           :src="quiz.img"
           alt=""
@@ -25,13 +32,11 @@ watch(search, () =>{
         <div class="card-text">
           <h2>{{quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+        </div> 
+      </div>-->
     </div>
   </div>
 </template>
-
-
 
 <style scoped>
 .container {
@@ -57,31 +62,11 @@ header input {
   border-radius: 5px;
 }
 
-.options-container{
+.options-container {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
 }
 
 /*CARD*/
-.card{
-  width: 300px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.1) ;
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-.card img{
-  width: 100%;
-  padding: 10px;
-  margin: 0;
-}
-.card .card-text{
-  padding: 0 5px;
-}
-.card .card-text h2{
- font-weight: bold;
-}
 </style>
---
+++ QuizApp/src/components/Card.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { defineProps } from "vue";
+const { quiz } = defineProps(["quiz"]);
+// Passing Props to the Question Component
+
+// console.log(quiz);
+</script>
+
+<template>
+  <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img
+          :src="quiz.img"
+          alt=""
+        />
+        <div class="card-text">
+          <h2>{{quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div> -->
+  <div class="card">
+    <img :src="quiz.img" alt="" />
+    <div class="card-text">
+      <h2>{{ quiz.name }}</h2>
+      <p>{{ quiz.questions.length }} questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 300px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.card img {
+  width: 100%;
+  padding: 10px;
+  margin: 0;
+}
+.card .card-text {
+  padding: 0 5px;
+}
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
