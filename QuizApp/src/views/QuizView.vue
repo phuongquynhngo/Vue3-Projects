b762ee33e149cbe63694817b243ba45ec906a459
Quynh <ngophuongquynh25@gmail.com>
2023-03-05 11:55:54 +0100

    Separating Our Code Into Components to get quizId

--- QuizApp/src/views/QuizView.vue
+++ QuizApp/src/views/QuizView.vue
@@ -1,9 +1,16 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import {useRoute} from "vue-router";
+
+const route=useRoute()
+
+const quizId= parseInt(route.params.id)
 </script>
 <template>
   <div>
+
+  {{ quizId }}
     <QuizHeader />
     <div>
       <Question />
