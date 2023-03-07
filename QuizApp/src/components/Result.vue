02a46b57ae3cc711771bb6418f089ad417b099c5
Quynh <ngophuongquynh25@gmail.com>
2023-03-07 14:19:38 +0100

    display Result Component

---
+++ QuizApp/src/components/Result.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { numberOfCorrectAnswers, quizQuestionsLength } = defineProps([
+  "numberOfCorrectAnswers",
+  "quizQuestionsLength",
+]);
+</script>
+
+<template>
+  <div class="results">
+    <p>Your Results ...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionsLength }}</h1>
+    <button>Go back</button>
+  </div>
+  
+</template>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0px;
+}
+p {
+  font-size: 25px;
+}
+h1 {
+  font-size: 48px;
+}
+</style>
--- QuizApp/src/views/QuizView.vue
+++ QuizApp/src/views/QuizView.vue
@@ -1,18 +1,18 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import Result from "../components/Result.vue";
+
 import { useRoute } from "vue-router";
 import { ref, watch, computed } from "vue";
 import quizes from "../data/quizes.json";
 
 const route = useRoute();
-
 const quizId = parseInt(route.params.id);
-
 const quiz = quizes.find((q) => q.id === quizId);
-
 const currentQuestionIndex = ref(0);
 const numberOfCorrectAnswers =ref(0);
+const showResult=ref(false)
 // const questionStatus= ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
 // watch (() => currentQuestionIndex.value, ()=>{
@@ -50,6 +50,9 @@ const onOptionSlected=(isCorrect)=>{
   if(isCorrect){
     numberOfCorrectAnswers.value++;
   }
+  if(currentQuestionIndex.value===quiz.questions.length-1){
+    showResult.value=true
+  }
   currentQuestionIndex.value++;
 }
 
@@ -63,9 +66,15 @@ const onOptionSlected=(isCorrect)=>{
       :barPercentage="barPercentage"
     />
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]" 
+      <Question 
+      v-if="!showResult"
+      :question="quiz.questions[currentQuestionIndex]" 
       @selectOption="onOptionSlected" />
+      <Result v-else
+      :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      :quizQuestionsLength="quiz.questions.length"/>
     </div>
-    <button @click="currentQuestionIndex++">Next question</button>
+    <!-- <button @click="currentQuestionIndex++">Next question</button> -->
+
   </div>
 </template>
